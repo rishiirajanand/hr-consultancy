922d03ae11f94f294afe0e6cd32c74545e8de10b
ankushh19 <ak9108824@gmail.com>
2024-03-30 11:58:11 +0530

    add navbar

---
+++ script.js
@@ -0,0 +1,23 @@
+// Navbar
+function setActive(element) {
+  let lis = document.querySelectorAll(".nav-content ul li");
+  lis.forEach(function (li) {
+    li.classList.remove("active");
+  });
+  element.parentElement.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let homeLink = document.querySelector(".nav-content ul li.active");
+  if (!homeLink) {
+    let defaultActiveLink = document.querySelector(
+      ".nav-content ul li:first-child"
+    );
+    defaultActiveLink.classList.add("active");
+  }
+});
+
+function toggleDropdown(element, isHovering) {
+  const dropdownContent = element.querySelector(".dropdown-content");
+  dropdownContent.style.display = isHovering ? "flex" : "none";
+}
